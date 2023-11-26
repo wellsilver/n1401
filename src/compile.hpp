cf44efb26571eba96e46dfa865f7a5bfe26d68ee
wellsilver <wellsilver2016@gmail.com>
2023-11-25 19:54:20 -0500

    clean parse

--- src/compile.hpp
+++ src/compile.hpp
@@ -23,34 +23,35 @@ string readuntill(char v, string *f) {
 
 string compiletocode(string f) {
   string binary;
-  
-  int lines = 0;
-  for (auto c : f)
-    if (c=='\n') lines++;
-  cout << f << endl;
-  bool valid = false;
-  string instr = "";
-  string a = "";
-  bool hasa = false;
-  string b = "";
-  bool hasb = false;
-  string d = "";
-  bool hasd = false;
-  vector<struct instruction> instructions = instructionlist();
-  
-  for (int loop=0; loop<lines;loop++) {
-    hasa=false, hasb = false, hasd = false;
-    instr = readuntill(',', &f);
-    if (instr[instr.length()] == ':') continue;
-    for (auto i : instructions) {
-      if (i.name == instr && i.hasA == hasa && i.hasB == hasb && i.hasD == hasd) valid = true;
+
+  vector<vector<string>> lines;
+  string arg = "";
+  vector<string> args;
+
+  f+='\n';
+  for (auto c : f) {
+    if (c == ':') {
+      arg = "";
+      continue;
     }
-    if (!valid) {
-      printf("\e[31m[Error] Instruction does not exist (check its arguements) \"%s\"\n\e[0m", instr.data());
-      exit(-2);
+    if (c == '\n') {
+      args.push_back(arg);
+      if (arg != "")
+        lines.push_back(args);
+      args.clear();
+      arg = "";
+      continue;
     }
+    if (c == ',') {
+      args.push_back(arg);
+      arg = "";
+      continue;
+    }
+    arg += c;
   }
-  
+
+  vector<struct instruction> instructions = instructionlist();
+
   return binary;
 }
 
