47df4a9c4724b3deb46e98ca3dbfac9f2166b6ff
wellsilver <wellsilver2016@gmail.com>
2024-04-26 22:09:53 -0400

    add a newline

--- src/compile.hpp
+++ src/compile.hpp
@@ -95,6 +95,7 @@ string compiletotape(string f) {
     /// find the instruction in the instruction list then write its binary
     for (auto i : alli) if (i.name == name && i.hasA == hasA && i.hasB == hasB && i.hasD == hasD && i.AisFX == AisFX)
       std::cout << "Correct instruction! " << name << endl;
+      
   }
 
   return binary;
